c8375127a0cab9e0fb47311f5f818ea544fa3b77
eirsture <eirik.sture@gmail.com>
2019-11-23 13:20:50 +0100

    Added a file to test the arduino temp readings

---
+++ arduino-test.py
@@ -0,0 +1,22 @@
+import serial, time
+from datetime import datetime
+
+
+# The port that the arduino can be reached/read at
+port = '/dev/ttyUSB0'
+
+# Enables reading the serial signal
+arduino = serial.Serial(port,9600,timeout=None)
+time.sleep(10) # wait for Arduino
+
+i = 0
+
+oldDate = datetime.now()
+newDate = None
+
+while i < 5:
+    temp = float(arduino.readline().strip())
+    if temp:
+        print(temp)
+        i += 1;
+    time.sleep(10)
