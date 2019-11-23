cf6c098ffdc676a15dbbb17a7ddc8dbf4d7838a1
eirsture <eirik.sture@gmail.com>
2019-11-23 13:16:11 +0100

    Changed path to the serviceAccountKey credentials

--- temperatures.py
+++ temperatures.py
@@ -10,7 +10,7 @@ from datetime import datetime
 import firebase_admin
 from firebase_admin import credentials, firestore
 
-cred = credentials.Certificate("path/to/serviceAccountKey.json")
+cred = credentials.Certificate("./serviceAccountKey.json")
 firebase_admin.initialize_app(cred)
 
 db = firestore.client()
